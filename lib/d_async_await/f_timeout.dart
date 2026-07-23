b1564e13ca0dc305d31ab260f0186d8f7069c1da
renatoalves <renatorv@gmaail.com>
2026-07-22 21:57:46 -0300

    print(DateTime.now().toIso8601String());

---
+++ lib/d_async_await/f_timeout.dart
@@ -0,0 +1,33 @@
+import 'dart:async';
+
+void main() {
+  final aguardando = inseringoAlgoBemLento();
+
+  print(DateTime.now().toIso8601String());
+
+  // aguardando
+  //     .timeout(
+  //       Duration(seconds: 1),
+  //       onTimeout: () {
+  //         print('Terminou o timeout...');
+  //       },
+  //     )
+  //     .then((value) {
+  //       print('Terminou o timeout');
+  //     })
+  //     .catchError((erro) {
+  //       print('Terminou com erro: $erro');
+  //     });
+  try {
+    final aguardando = await inseringoAlgoBemLento().timeout(Duration(seconds: 1));
+  } on TimeoutException catch (e) {
+    print('Finalizou com timeout: $e');
+  }
+}
+
+Future<void> inseringoAlgoBemLento() {
+  return Future.delayed(Duration(seconds: 3), () {
+    print('Terminou o insert');
+    print(DateTime.now().toIso8601String());
+  });
+}
