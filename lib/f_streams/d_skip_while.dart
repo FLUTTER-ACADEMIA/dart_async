9ea37eb317ed2fbf729835f2c4edcb1a4b2c5084
renatoalves <renatorv@gmaail.com>
2026-07-30 22:27:24 -0300

    Listen

---
+++ lib/f_streams/d_skip_while.dart
@@ -0,0 +1,26 @@
+Future<void> main() async {
+  print('Inicio...');
+  final interval = Duration(seconds: 1);
+
+  var stream = Stream<int>.periodic(interval, callback);
+
+  // skip(2) pula os dois primeiros
+  stream = stream.take(5).skipWhile((value) {
+    print('Número que chegou na stream $value');
+    return value < 5;
+  });
+
+  print('passou aqui');
+
+  // await for
+  await for (var i in stream) {
+    print('O número que chegou no await for $i');
+  }
+
+  print('Fim...');
+}
+
+int callback(int value) {
+  print('Valor: $value');
+  return (value + 1) * 2;
+}
